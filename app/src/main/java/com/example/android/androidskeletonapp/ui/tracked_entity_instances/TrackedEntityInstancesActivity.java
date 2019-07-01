8d268b1f466395d1dcdc21b7a5c3cb2c81ca5af2
Marcos Campos <marcamsn@gmail.com>
2019-07-01 20:43:51 +0200

    ex07-teiList

--- app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstancesActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstancesActivity.java
@@ -3,20 +3,11 @@ package com.example.android.androidskeletonapp.ui.tracked_entity_instances;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.android.androidskeletonapp.R;
-import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceCollectionRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import io.reactivex.disposables.CompositeDisposable;
-
-import static android.text.TextUtils.isEmpty;
 
 public class TrackedEntityInstancesActivity extends ListActivity {
 
@@ -40,38 +31,7 @@ public class TrackedEntityInstancesActivity extends ListActivity {
                 R.id.trackedEntityInstancesRecyclerView);
         selectedProgram = getIntent().getStringExtra(IntentExtra.PROGRAM.name());
         compositeDisposable = new CompositeDisposable();
-        observeTrackedEntityInstances();
-
-        if (isEmpty(selectedProgram))
-            findViewById(R.id.enrollmentButton).setVisibility(View.GONE);
-
-        findViewById(R.id.enrollmentButton).setOnClickListener(view -> {
-            // TODO Create tracked entity instance
-            }
-        );
-    }
-
-    private void observeTrackedEntityInstances() {
-        TrackedEntityInstanceAdapter adapter = new TrackedEntityInstanceAdapter();
-        recyclerView.setAdapter(adapter);
-
-        getTeiRepository().getPaged(20).observe(this, trackedEntityInstancePagedList -> {
-            adapter.submitList(trackedEntityInstancePagedList);
-            findViewById(R.id.trackedEntityInstancesNotificator).setVisibility(
-                    trackedEntityInstancePagedList.isEmpty() ? View.VISIBLE : View.GONE);
-        });
-    }
-
-    private TrackedEntityInstanceCollectionRepository getTeiRepository() {
-        TrackedEntityInstanceCollectionRepository teiRepository =
-                Sdk.d2().trackedEntityModule().trackedEntityInstances.withTrackedEntityAttributeValues();
-        if (!isEmpty(selectedProgram)) {
-            List<String> programUids = new ArrayList<>();
-            programUids.add(selectedProgram);
-            return teiRepository.byProgramUids(programUids);
-        } else {
-            return teiRepository;
-        }
+        // TODO create TEI list
     }
 
     @Override
