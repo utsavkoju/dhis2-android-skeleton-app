64a6eee3d0d5691ec997b989aadb17976fe524c4
Marcos Campos <marcamsn@gmail.com>
2019-07-01 17:52:32 +0200

    ex19-dataSets

--- app/src/main/java/com/example/android/androidskeletonapp/ui/data_sets/DataSetsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/data_sets/DataSetsActivity.java
@@ -1,16 +1,9 @@
 package com.example.android.androidskeletonapp.ui.data_sets;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.android.androidskeletonapp.R;
-import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
-
-import org.hisp.dhis.android.core.dataset.DataSet;
-
-import androidx.lifecycle.LiveData;
-import androidx.paging.PagedList;
 
 public class DataSetsActivity extends ListActivity {
 
@@ -18,21 +11,6 @@ public class DataSetsActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_data_sets, R.id.dataSetsToolbar, R.id.dataSetsRecyclerView);
-        observeDataSets();
-    }
-
-    private void observeDataSets() {
-        DataSetsAdapter adapter = new DataSetsAdapter();
-        recyclerView.setAdapter(adapter);
-
-        LiveData<PagedList<DataSet>> liveData = Sdk.d2().dataSetModule().dataSets
-                .withStyle()
-                .getPaged(20);
-
-        liveData.observe(this, dataSetPagedList -> {
-            adapter.submitList(dataSetPagedList);
-            findViewById(R.id.dataSetsNotificator).setVisibility(
-                    dataSetPagedList.isEmpty() ? View.VISIBLE : View.GONE);
-        });
+        // TODO List DataSets
     }
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
@@ -11,7 +11,6 @@ import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.data.service.ActivityStarter;
 import com.example.android.androidskeletonapp.data.service.SyncStatusHelper;
 import com.example.android.androidskeletonapp.ui.d2_errors.D2ErrorActivity;
-import com.example.android.androidskeletonapp.ui.data_sets.DataSetsActivity;
 import com.example.android.androidskeletonapp.ui.foreign_key_violations.ForeignKeyViolationsActivity;
 import com.example.android.androidskeletonapp.ui.programs.ProgramsActivity;
 import com.example.android.androidskeletonapp.ui.tracked_entity_instances.TrackedEntityInstancesActivity;
@@ -260,7 +259,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        // TODO Launch data set reports activity
+        // TODO Launch data set activity
 
         if (id == R.id.navPrograms) {
             ActivityStarter.startActivity(this, ProgramsActivity.class,false);
@@ -268,8 +267,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             ActivityStarter.startActivity(this, TrackedEntityInstancesActivity.class,false);
         } else if (id == R.id.navTrackedEntitiesSearch) {
             ActivityStarter.startActivity(this, TrackedEntityInstanceSearchActivity.class,false);
-        } else if (id == R.id.navDataSets) {
-            ActivityStarter.startActivity(this, DataSetsActivity.class,false);
         } else if (id == R.id.navD2Errors) {
             ActivityStarter.startActivity(this, D2ErrorActivity.class,false);
         } else if (id == R.id.navFKViolations) {
