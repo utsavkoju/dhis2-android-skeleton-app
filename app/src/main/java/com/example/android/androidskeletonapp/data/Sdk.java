94119a15c8976889ade1561cfecb3f76eceb1ef6
Utsav Ashish Koju <utsavkoju@hotmail.com>
2019-07-02 12:20:41 +0200

    Solution d2

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -6,9 +6,18 @@ import org.hisp.dhis.android.core.d2manager.D2Configuration;
 
 public class Sdk {
 
-    public static D2Configuration getD2Configuration(Context context) {
-        // TODO Add configuration
+    static String app_name = "DHIS2 Capture";
+    static String app_version = "0.0.1";
+    static Integer time_out = 60;
 
-        return null;
+    public static D2Configuration getD2Configuration(Context context) {
+        D2Configuration config = D2Configuration.builder()
+                .appName(app_name)
+                .appVersion(app_version)
+                .readTimeoutInSeconds(time_out).connectTimeoutInSeconds(time_out).writeTimeoutInSeconds(time_out)
+                .context(context)
+                .build();
+
+        return config;
     }
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.androidskeletonapp.ui.splash;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 
 import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Configuration;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Single;
@@ -13,6 +18,8 @@ import io.reactivex.disposables.Disposable;
 public class SplashActivity extends AppCompatActivity {
 
     private Disposable disposable;
+    static String serverUrl = "http://android2.dhis2.org:8080";
+    static String TAG = "SplashActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +28,16 @@ public class SplashActivity extends AppCompatActivity {
 
         disposable =
                 instantiateD2()
-                .doOnSuccess(d2 -> {
-                    // TODO Toast success
-                    }
-                ).doOnError(throwable -> {
-                    // TODO Toast error
+                        .doOnSuccess(d2 -> {
+                                    Log.e(TAG,"Testing if the successful");
+                                    Toast.makeText(getApplicationContext(),"Successful in D2 Configuration", Toast.LENGTH_LONG).show();
+                                }
+                        ).doOnError(throwable -> {
+                    Log.e(TAG, "Failure");
+                    Toast.makeText(getApplicationContext(),"Error in D2 Configuration", Toast.LENGTH_LONG).show();
                 })
-                .subscribe();
+                        .subscribe();
+        Log.e(TAG, "Outside");
     }
 
     @Override
@@ -40,6 +50,10 @@ public class SplashActivity extends AppCompatActivity {
 
     private Single<D2> instantiateD2() {
         // TODO Instantiate d2
-        return Single.never();
+        D2Configuration config = Sdk.getD2Configuration(this);
+        Single<D2> d2Single = D2Manager.setUp(config)
+                .andThen(D2Manager.setServerUrl(serverUrl))
+                .andThen(D2Manager.instantiateD2());
+        return d2Single;
     }
 }
