3ba888703c284ce58fc021124d07f526f03fb973
Utsav Ashish Koju <utsavkoju@hotmail.com>
2019-07-02 18:01:50 +0200

    Solution Program with Orgunits Filter

--- app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
@@ -7,11 +7,16 @@ import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 
+import org.hisp.dhis.android.core.arch.helpers.UidsHelper;
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
 import org.hisp.dhis.android.core.program.Program;
 import org.hisp.dhis.android.core.program.ProgramType;
+import org.hisp.dhis.android.core.user.User;
 
 import androidx.lifecycle.LiveData;
 import androidx.paging.PagedList;
+
+import java.util.List;
 
 public class ProgramsActivity extends ListActivity implements OnProgramSelectionListener {
 
@@ -41,6 +46,22 @@ public class ProgramsActivity extends ListActivity implements OnProgramSelection
 
     @Override
     public void onProgramSelected(String programUid, ProgramType programType) {
+        ProgramsAdapter adapter = new ProgramsAdapter(this);
+        recyclerView.setAdapter(adapter);
 
+        List<OrganisationUnit>orgUnits = Sdk.d2().organisationUnitModule().organisationUnits
+                .byOrganisationUnitScope(OrganisationUnit.Scope.SCOPE_DATA_CAPTURE).get();
+        List<String> orgUnitUids = UidsHelper.getUidsList(orgUnits);
+
+        LiveData<PagedList<Program>> programs = Sdk.d2().programModule().programs
+                .withStyle()
+                .byOrganisationUnitList(orgUnitUids)
+                .getPaged(20);
+
+        programs.observe(this, programPagedList -> {
+            adapter.submitList(programPagedList);
+            findViewById(R.id.programsNotificator).setVisibility(
+                    programPagedList.isEmpty() ? View.VISIBLE : View.GONE);
+        });
     }
 }
